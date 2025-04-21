4b28d6aad0a0d0e1f022d5237fd747062b65ffee
Juan Pablo Tabares Gallego <juancho9615@hotmail.com>
2025-04-21 11:26:42 -0500

    if thymeleaf and foreach

--- src/main/java/com/juanpablo/curso/springboot/webapp/springboot_web/controllers/UserController.java
+++ src/main/java/com/juanpablo/curso/springboot/webapp/springboot_web/controllers/UserController.java
@@ -1,10 +1,15 @@
 package com.juanpablo.curso.springboot.webapp.springboot_web.controllers;
 
+import java.util.Arrays;
+import java.util.List;
+
 //import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 
 import com.juanpablo.curso.springboot.webapp.springboot_web.models.User;
 
@@ -14,7 +19,7 @@ public class UserController {
     @GetMapping("/details")
     public String details(Model model) {
 
-        User user = new User("Juan", "Tabares", "juan@tabares.com", "123");
+        User user = new User("Juan", "Tabares", "", "123");
 
         model.addAttribute("title", "Hola mundo Spring Boot");
         model.addAttribute("userInfo", user);
@@ -30,4 +35,21 @@ public class UserController {
      * return "details";
      * }
      */
+
+    @GetMapping("/list")
+    public String list(ModelMap model) {
+
+        model.addAttribute("title", "Lista de usuarios");
+
+        return "list";
+    }
+    @ModelAttribute("users")
+    public List<User> usersModel() {
+        return Arrays.asList(
+                new User("Juan", "Tabares", "", "123"),
+                new User("Pablo", "Gallego", "email@email.com", "123"),
+                new User("Juan Pablo", "Tabares", "", "123"));
+
+    }
+
 }
--- src/main/java/com/juanpablo/curso/springboot/webapp/springboot_web/controllers/UserRestController.java
+++ src/main/java/com/juanpablo/curso/springboot/webapp/springboot_web/controllers/UserRestController.java
@@ -1,6 +1,9 @@
 package com.juanpablo.curso.springboot.webapp.springboot_web.controllers;
 
+/* import java.util.ArrayList; */
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,6 +11,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.juanpablo.curso.springboot.webapp.springboot_web.models.User;
+import com.juanpablo.curso.springboot.webapp.springboot_web.models.dto.UserDto;
+/* import org.springframework.web.bind.annotation.RequestParam; */
+
 
 @RestController
 @RequestMapping("/api")
@@ -25,5 +31,32 @@ public class UserRestController {
 
         return bodyResponse;
     }
+
+    @GetMapping("/details-v2")
+    public UserDto detailsV2() {
+
+        User user = new User("Pablo", "Gallego", "pablo@gallego.com", "123");
+        UserDto userDto = new UserDto("Hola mundo Spring Boot", user);
+
+        return userDto;
+    }
+
+    @GetMapping("/list")      
+    public List<User> List(){
+        User user = new User("Juan", "Tabares", "juan@tabares.com", "123");
+        User user2 = new User("Pablo", "Gallego", "pablo@gallego.com", "123");
+        User user3 = new User("Sergio", "Garcia", "sergio@garcia.com", "123");
+
+        List<User> users = Arrays.asList(user, user2, user3);
+
+        /* List<User> users = new ArrayList<>();
+        users.add(user);
+        users.add(user2);
+        users.add(user3); */
+
+        return users;    
+    };
+
+    
 
 }
